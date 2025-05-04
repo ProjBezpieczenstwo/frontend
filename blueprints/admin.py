fc0b979b77a6307336c44a3fd52098e0eba976e8
Igor Osiakoski <igorzasty2@wp.pl>
2025-05-04 16:33:46 +0200

    Revert "please do not hurt me"
    This reverts commit 2f83bc1bf1bccaba036dee12e33c1404aa0144c2.

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -96,4 +96,8 @@ def delete_user_page(user_id):
 
     return redirect(url_for('admin.users_page'))
 
-
+@admin_bp.template_filter('format_datetime')
+def format_datetime(value, fmt="%d.%m.%Y %H:%M"):
+    if value is None:
+        return ""
+    return value.strftime(fmt)
